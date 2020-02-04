7bd8da1e28855d6baa79a3fc0279bcbded8d7ee5
Phillip Abell <pbabell@wpi.edu>
2020-02-04 17:26:24 -0500

    Tweaked Some angles
    added a variable to be able to adjust the release angle. I think this metric will be what needs the most tweaking during prototyping

--- PaddleWheel.groovy
+++ PaddleWheel.groovy
@@ -1,13 +1,13 @@
-int wheelRadius = 75
-int wheelHeight = 50
-
-CSG base = new Cylinder(wheelRadius - 3,wheelRadius - 3,5,(int)30).toCSG()
+float wheelRadius = 75
+float wheelHeight = 50
+int releaseAngle = 10
+CSG base = new Cylinder(wheelRadius-3,wheelRadius-3,5,(int)30).toCSG()
 CSG innerVoid = base.scalez(10)
 CSG outerWall = new Cylinder(wheelRadius,wheelRadius,50,(int)30).toCSG().difference(innerVoid)//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
-CSG scoopInner = new Cylinder(9,9,50,(int)30).toCSG()
-CSG scoopOuter = new Cylinder(12,12,50,(int)30).toCSG()
+CSG scoopInner = new Cylinder(9,9,wheelHeight+5,(int)30).toCSG()
+CSG scoopOuter = new Cylinder(12,12,wheelHeight+5,(int)30).toCSG()
 CSG cutPlane = new Cube(40,20,60).toCSG().movey(-10).toZMin()
-CSG scoopCombined = scoopOuter.difference(scoopInner).difference(cutPlane).rotx(-10).roty(-3).movex(wheelRadius-13)
+CSG scoopCombined = scoopOuter.difference(scoopInner).difference(cutPlane).rotz(releaseAngle).rotx(-10).roty(-3).movex(wheelRadius-13)
 CSG wheel = base.union(outerWall)
 innerVoid = innerVoid.scalex(0.5).scaley(0.5)
 CSG cleanMax = new Cylinder(wheelRadius,wheelRadius,wheelHeight,(int)30).toCSG()
