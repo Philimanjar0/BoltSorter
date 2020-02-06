b141adcc1d62c67b77f11b0adc7aa8cccd5ef721
phil <phil@phil-Lenovo-YOGA-720-13IKB>
2020-02-06 10:41:39 -0500

    added gear, variables, comments
    I added the gear to the outside of the drum. Needed to drive the drum.
    I added more variables for future tweaking.
    I added comments for future readability.

--- PaddleWheel.groovy
+++ PaddleWheel.groovy
@@ -1,27 +1,61 @@
-float wheelRadius = 75
-float wheelHeight = 50
-int releaseAngle = 10
-int wallThickness = 2
-tollerance = 0
-CSG m5Bearing = Vitamins.get("ballBearing", "695zz")
-CSG bearingCylinder = new Cylinder(m5Bearing.getMaxY() + tollerance, m5Bearing.getMaxY() + tollerance,5,(int)30).toCSG()
+float wheelRadius = 75 //outer radius of the drum
+float wheelHeight = 50 //height of the drum
+int releaseAngle = 10 //the z angle the scoops are turned, changes when bolts are dropped
+int wallThickness = 2 //thickness of wall of drum and scoops
+int gearPitch = 6 //pitch of gear teeth, defined by arc length per gear tooth
+int tollerance = 0 //to be assigned later
+int numberOfScoops = 8 //number of scoops in the wheel
+double toothDepth = 1/(Math.PI/gearPitch) + 2/(Math.PI/gearPitch) //sum of addendum and dedendum
 
-CSG base = new Cylinder(wheelRadius-wallThickness,wheelRadius-wallThickness,5,(int)30).toCSG()
+CSG m5Bearing = Vitamins.get("ballBearing", "695zz") //bearing vitamin
+CSG bearingCylinder = new Cylinder(m5Bearing.getMaxY() + tollerance, m5Bearing.getMaxY() + tollerance,5,(int)30).toCSG() //cyinder used for bearing slots, NO TOLLERANCE
+
+CSG base = new Cylinder(wheelRadius-wallThickness,wheelRadius-wallThickness,5,(int)30).toCSG() //back plate of the  wheel
+
+//create the gears from a different library
+def bevelGears = ScriptingEngine.gitScriptRun(
+            "https://github.com/madhephaestus/GearGenerator.git", // git location of the library
+            "bevelGear.groovy" , // file to load
+            // Parameters passed to the funcetion
+            [(150+toothDepth)*Math.PI/gearPitch,// Number of teeth gear a
+	        24,// Number of teeth gear b
+	        6,// thickness of gear A
+	        6,// gear pitch in arc length mm
+	        0,// shaft angle, can be from 0 to 100 degrees
+	        0// helical angle, only used for 0 degree bevels
+            ]
+            )
+
+//create the outside wall
 CSG innerVoid = base.scalez(10)
-CSG outerWall = new Cylinder(wheelRadius,wheelRadius,50,(int)70).toCSG().difference(innerVoid)//convert to CSG to display                    			         ).toCSG()//convert to CSG to display 
+CSG outerWall = new Cylinder(wheelRadius,wheelRadius,50,(int)70).toCSG().difference(innerVoid)
+
+//create and rotate the first scoop
 CSG scoopInner = new Cylinder(10,10,wheelHeight+5,(int)30).toCSG()
 CSG scoopOuter = new Cylinder(12,12,wheelHeight+5,(int)30).toCSG()
 CSG cutPlane = new Cube(40,20,60).toCSG().movey(-10).toZMin()
 CSG scoopCombined = scoopOuter.difference(scoopInner).difference(cutPlane).rotz(releaseAngle).rotx(-10).roty(-3).movex(wheelRadius-12)
-CSG wheel = base.union(outerWall)
-for(int i=0; i<8; i++){
-	wheel=wheel.union(scoopCombined.rotz(i*(360/8)))
-}
-wheel=wheel.intersect(new Cylinder(wheelRadius,wheelRadius,wheelHeight,(int)70).toCSG())
 
-CSG bearingSupport = new Cylinder(15,10,15,(int)30).toCSG()
+
+CSG wheel = base.union(outerWall) //combine base with outter wall
+
+//iteratively add the scoops to the wheel
+for(int i=0; i<numberOfScoops; i++){
+	wheel=wheel.union(scoopCombined.rotz(i*(360/numberOfScoops)))
+}
+
+wheel=wheel.intersect(new Cylinder(wheelRadius,wheelRadius,wheelHeight,(int)70).toCSG()) //remove anything protruding from the drum
+wheel = wheel.union(bevelGears.get(0)) //add the gear teeth to the outside of the drum
+
+//add bearing support on the inside of the drum
+CSG bearingSupport = new Cylinder(15,10,15,(int)30).toCSG() 
 wheel = wheel.union(bearingSupport)
+
+//hole for bearings to sit
 wheel = wheel.difference(bearingCylinder.movez(10))
 wheel = wheel.difference(bearingCylinder)
-wheel = wheel.difference(new Cylinder(2.5 + tollerance,2.5 + tollerance,20,(int)30).toCSG())
+
+//TODO: us m5 vitamin
+wheel = wheel.difference(new Cylinder(2.5 + tollerance,2.5 + tollerance,20,(int)30).toCSG()) //hole for m5 bolt
+
 return wheel
